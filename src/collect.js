e9fee2eacb166e701460d10f53da8330ef59d017
刘娟娟 <liu.juanjuan@xyb2b.com>
2023-08-18 18:18:43 +0800

    feat: EXP曝光

--- src/collect.js
+++ src/collect.js
@@ -1,5 +1,5 @@
 import qs from 'qs';
-import { upload } from "./upload";
+import { upload } from './upload';
 
 /**
  * 参数创建前的钩子函数
@@ -16,16 +16,16 @@ let afterUpload;
 /**
  * 报错的钩子函数
  */
-let onError = function(e){
-	console.error(e);
-}
+let onError = function (e) {
+  console.error(e);
+};
 
 /**
  * 发送PV日志
  */
-export function sendPV() {
-	let appID, pageID, timestamp, ua;
-	beforeCreateParams && beforeCreateParams();
+export function collect(data, options) {
+  let appID, pageID, timestamp, ua;
+  beforeCreateParams && beforeCreateParams();
   // 采集页面的基本信息
   // 1、应用（meta标签下的app-id）
   // 2、页面（body标签下的page-id）
@@ -34,7 +34,7 @@ export function sendPV() {
     let meta = metaList[i];
     if (meta.getAttribute('app-id')) {
       appID = meta.getAttribute('app-id');
-			break;
+      break;
     }
   }
   const body = document.body;
@@ -46,46 +46,53 @@ export function sendPV() {
   // 1、应用ID&页面ID
   // 2、用户的访问时间
   // 3、用户的信息
-	timestamp = new Date().getTime();
-	ua = window.window.navigator.userAgent;
+  timestamp = new Date().getTime();
+  ua = window.window.navigator.userAgent;
   // 调用日志上报API
-	let data = {
-		appID,
-		pageID,
-		timestamp,
-		ua
-	}
-	data = qs.stringify(data);
-	if (beforeUpload){
-		data = beforeUpload(data);
-	}
-	let url, uploadData;
-	try{
-		const result = upload(data);
-		url = result.url;
-		uploadData = result.data;
-	} catch(e){
-		onError(e);
-	} finally{
-		console.log('1111', 1111);
-		afterUpload && afterUpload(url, uploadData)
-	}
+  let params = {
+    appID,
+    pageID,
+    timestamp,
+    ua,
+    ...data,
+  };
+  params = qs.stringify(params);
+  if (beforeUpload) {
+    params = beforeUpload(params);
+  }
+  let url, uploadData;
+  try {
+    const result = upload(params, options);
+    url = result.url;
+    uploadData = result.data;
+  } catch (e) {
+    onError(e);
+  } finally {
+    afterUpload && afterUpload(url, uploadData);
+  }
 }
 
-
-export function registerBeforeCreateParams(fn){
-	beforeCreateParams = fn;
+export function sendPV() {
+  collect({}, { eventType: 'PV' });
 }
 
-export function registerBeforeUpload(fn){
-	beforeUpload = fn;
+export function sendEXP(data) {
+  collect(data, { eventType: 'EXP' });
 }
 
-export function registerAfterUpload(fn){
-	afterUpload = fn;
+export function registerBeforeCreateParams(fn) {
+  beforeCreateParams = fn;
 }
 
-export function registerOnError(fn){
-	onError = fn;
+export function registerBeforeUpload(fn) {
+  beforeUpload = fn;
+}
+
+export function registerAfterUpload(fn) {
+  afterUpload = fn;
+}
+
+export function registerOnError(fn) {
+  onError = fn;
 }
 export default {};
--- src/index.js
+++ src/index.js
@@ -7,6 +7,24 @@ import {
 } from './collect';
 import { upload } from './upload';
 
+const appearEvent = new CustomEvent('onAppear');
+const disappearEvent = new CustomEvent('onDisappear');
+const ob = new window.IntersectionObserver(function(entries){
+  entries.forEach(item=>{
+    if (item.target.intersectionRatio > 0) {
+      console.log('显示了');
+      item.target.dispatchEvent(appearEvent);
+    } else {
+      console.log('消失了');
+      item.target.dispatchEvent(disappearEvent);
+    }
+  })
+});
+const appearDom = document.querySelectorAll('[appear]');
+appearDom.forEach(item => {
+  ob.observe(item);
+})
+
 window.__monitor = {
   sendPV,
   registerBeforeCreateParams, 
